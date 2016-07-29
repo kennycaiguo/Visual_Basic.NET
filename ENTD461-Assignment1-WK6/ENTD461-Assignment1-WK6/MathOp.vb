254a0bb870d1041ddb4a728fe8a5d36e60459d0a
Wilman Gonzales <wilmangonzales@rocketmail.com>
2016-07-29 02:40:56 -0400

    Add files via upload

---
+++ ENTD461-Assignment1-WK6/ENTD461-Assignment1-WK6/MathOp.vb
@@ -0,0 +1,23 @@
+﻿'Wilman Gonzales
+'Class MathOp
+
+'this is the MathOP class
+
+Public Class MathOp
+    Protected Num1 As Decimal
+    Protected Num2 As Decimal
+
+    Public Sub New(n1 As Decimal, n2 As Decimal)
+        Num1 = n1
+        Num2 = n2
+    End Sub
+    ' this is the add function
+    Public Function Add() As Decimal
+        Return Num1 + Num2
+    End Function
+    ' this is the subtract funtion
+    Public Function Subtract() As Decimal
+        Return Num1 - Num2
+    End Function
+
+End Class
---
+++ ENTD461-Assignment1-WK6/ENTD461-Assignment1-WK6/MathOp2.vb
@@ -0,0 +1,22 @@
+﻿'Wilman Gonzales
+
+
+'this is the MathOP2 class
+
+
+Public Class MathOp2
+    Inherits MathOp
+
+    Public Sub New(n1 As Decimal, n2 As Decimal)
+        ' Call the base class constructor to store n1 and n2. 
+        MyBase.New(n1, n2)
+    End Sub
+    'this is the Multiply function
+    Public Function Multiply() As Decimal
+        Return Num1 * Num2
+    End Function
+    ' this is the division function
+    Public Function Divide() As Decimal
+        Return Num1 / Num2
+    End Function
+End Class
